d8214a6d8f8624e569b48c50eb25068db3ed05a6
Bhagyshree Vishwakarma <167774877+shreegitub@users.noreply.github.com>
2024-08-15 20:37:50 +0530

    Update Employee data projects.sql

--- Employee data projects.sql
+++ Employee data projects.sql
@@ -68,50 +68,133 @@ select * from incentive;
 -- use forign key
 alter table Employee add foreign key (emp_id) references incentive(emp_id);
 
--- Display all the information of the Employee table
+-- Q1 Display all the information of the Employee table
 select * from Employee;
 
--- Display unique Department names from Employee table.
+-- Q2 Display unique Department names from Employee table.
 select distinct departent_name from Employee;
 
--- List the details of the employees in ascending order of their salaries.
+-- Q3 List the details of the employees in ascending order of their salaries.
 select * from Employee 
 order by salary asc;
 
--- List the employees who joined before 2014.
+-- Q4 List the employees who joined before 2014.
 select * from Employee
 where joining_date <"2014-01-01";
 
--- List the Empno, Ename, Sal, Daily Sal of all Employees in the ASC order of AnnSal.
+-- Q5 List the Empno, Ename, Sal, Daily Sal of all Employees in the ASC order of AnnSal.
 select emp_id, emp_name, salary, salary/30 Daily_Sal from Employee
 order by salary*12 asc;
 
--- List the employees who are working for the department name fesion or teaching
+-- Q6 List the employees who are working for the department name fesion or teaching
 select * from Employee
 where departent_name in ("fesion","teaching");
 
--- List the employees who are joined in the year 2015
+-- Q7 List the employees who are joined in the year 2015
 select * from Employee
 where joining_date = "2015-01-01";
 
--- List the employees who does not belong to department name teaching
+-- Q8 List the employees who does not belong to department name teaching
 select * from Employee
 where departent_name not in ("teaching");
 
--- select employee details from employee table if data exists in incentive table ?
+-- Q9 select employee details from employee table if data exists in incentive table ?
 select * from Employee e1 
 inner join incentive i 
 on e1.emp_id = i.emp_id;
 
--- Get Employee ID’s of those employees who didn’t receive incentives
+-- Q10 Get Employee ID’s of those employees who didn’t receive incentives
 select * from Employee e1
 where not exists (select * from incentive);
 
--- SQL Query to print the number of employees per department in the organization
+-- Q11 SQL Query to print the number of employees per department in the organization
 select emp_id , count(departent_name) from Employee 
 group by emp_id; 
 
--- **********************************************************************************************
+-- Q12 List employees who have not received any incentives.
+select * from employee; 
+select * from incentive; 
+select e.emp_id,e.emp_name from employee e 
+join incentive i 
+on e.emp_id = i.emp_id 
+where i.emp_id is null; 
+
+-- Q13 Calculate the total amount of incentives paid.
+select * from incentive; 
+select sum(incentive_amount) as total_amount from incentive;
+
+-- Q14 Update the salary of employee with emp_id 7 to 4000.
+select * from employee; 
+update employee
+set salary = 4000
+where emp_id = 7;
+
+-- Q15 List all employees along with their incentive 
+-- amounts in descending order of incentives.
+select * from employee ; 
+select * from incentive;
+select e.emp_id,e.emp_name,i.incentive_amount from employee e
+left join incentive i
+on e.emp_id = i.emp_id 
+order by i.incentive_amount desc;
+
+-- Q16 Find the number of employees in each department.
+select * from employee; 
+select departent_name, count(emp_id)as no_of_employee from employee
+group by departent_name; 
+
+-- Q17  Find the total salary expense of employees managed by manager with emp_id 2.
+select * from employee; 
+select sum(salary) as total_expense from employee 
+where MGR = 2; 
+
+-- Q18 List employees who have received incentives greater than 3000.
+select * from employee; 
+select * from incentive; 
+select * from employee e 
+join incentive i 
+on e.emp_id = i.emp_id 
+where incentive_amount>3000; 
+
+-- Q19 Retrieve the names of employees and the dates they received their 
+-- incentives, ordered by the incentive date.
+select * from employee; 
+select * from incentive; 
+select e.emp_name,i.incentive_date from employee e  
+join incentive i 
+on e.emp_id = i.emp_id 
+order by i.incentive_date ; 
+
+-- Q20 List the names of employees who received an incentive in the 
+-- year 2017 but do not work in the 'design' department.
+select * from employee ; 
+select * from incentive; 
+select * from employee e 
+join incentive i 
+on e.emp_id = i.emp_id 
+having departent_name <> 'design' or incentive_date = "2017-01-01" ;  
+
+-- Q21 Find the department with the highest average salary.
+select * from employee; 
+select * from incentive; 
+select departent_name,avg(salary) as highest_salary from employee 
+group by departent_name 
+order by avg(salary) desc;
+
+-- Q22 Find the total number of employees who have 
+-- received an incentive, grouped by their manager.
+select * from employee ; 
+select * from incentive ; 
+select count(e.emp_id) as total_employee from employee e  
+join incentive i 
+on e.emp_id = i.emp_id 
+group by MGR; 
+
+-- Q23 Retrieve the name and salary of employees who have a salary higher 
+-- than the average salary of employees in their own department.
+select emp_name,e.salary from employee e 
+where e.salary > ( select avg(salary) from employee);
+
 
 
 
